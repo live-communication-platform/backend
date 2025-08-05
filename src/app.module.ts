e6e99f7e4d70532336c373f4ede50ef795f143bd
Biruk GT <birukgebisa7@gmail.com>
2025-08-05 11:45:53 +0300

    removing meeting module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
-import { MeetingModule } from './meeting/meeting.module';
 import { MeetingGateway } from './meeting/meeting.gateway';
 
 @Module({
-  imports: [AuthModule, PrismaModule, MeetingModule],
+  imports: [AuthModule, PrismaModule],
   providers: [MeetingGateway],
 })
 export class AppModule {}
